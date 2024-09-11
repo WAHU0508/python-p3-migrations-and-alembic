fec41c5577fd5c6d527eba66bc3d0a7ab43036c9
WAHU0508 <sharongikenye@gmail.com>
2024-09-11 17:16:10 +0300

    Alembic and migrations

---
+++ app/migrations/versions/bc8147924475_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: bc8147924475
+Revises: 
+Create Date: 2024-09-11 17:01:41.141593
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc8147924475'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
